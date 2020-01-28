9254a7774ae24ee6fa521aa98f7524d6c2eaf51b
kentakuramochi <soratobi93@gmail.com>
2020-01-28 23:55:23 +0900

    add header

--- sort/insertion_sort.c
+++ sort/insertion_sort.c
@@ -2,8 +2,7 @@
 #include <string.h>
 #include <stdlib.h>
 
-// insertion sort
-void insertion_sort(void *array, const unsigned int size, const size_t n, int(*cmp)(const void*, const void*))
+void insertion_sort_val(void *array, const unsigned int size, const size_t n, int(*cmp)(const void*, const void*))
 {
     // tmp area for swap
     void *saved = malloc(size);
@@ -11,6 +10,7 @@ void insertion_sort(void *array, const unsigned int size, const size_t n, int(*c
     for (int i = 0; i < n; i++) {
         int j = i - 1;
         void *value = array + i * size;
+        // mark larger value than pivot
         while ((j >= 0) && (cmp((array + j * size), value) > 0)) {
             j--;
         }
@@ -18,6 +18,7 @@ void insertion_sort(void *array, const unsigned int size, const size_t n, int(*c
             continue;
         }
 
+        // swap values
         memmove(saved, value, size);
         memmove((array + (j + 1) * size), (array + j * size), (size * (j - 1)));
         memove((array + j * size), saved, size);
---
+++ sort/insertion_sort.h
@@ -0,0 +1,9 @@
+#ifndef INSERTION_SORT_H
+#define INSERTION_SORT_H
+
+#include <stddef.h>
+
+// insertion sort of values
+void insertion_sort_val(void *array, const unsigned int size, const size_t n, int(*cmp)(const void*, const void*))
+
+#endif
